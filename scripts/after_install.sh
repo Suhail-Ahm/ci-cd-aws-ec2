88d6816a2ca859fb29b775ab38d172089d092535
Suhail Ahmed <suhailahmed14369@mail.com>
2024-02-26 16:19:37 +0530

    fix

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Install dependencies
 apt-get update
-apt-get install -y python3-pip nginx
+apt-get install -y python3-pip python3-venv nginx
 
 # Install Gunicorn
-pip3 install gunicorn
+# pip3 install gunicorn
 
 # Configure Nginx
 # cp /var/www/fastapi_app/nginx.conf /etc/nginx/sites-available/fastapi_app
